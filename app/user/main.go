716e60b41f3b6634d0f75914ba64fd1cea0c0fd5
chenzhihan <chenzhihan@360.cn>
2021-09-18 15:28:28 +0800

    go-micro

--- app/user/main.go
+++ app/user/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 
+	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/web"
+	"github.com/micro/go-plugins/registry/etcdv3/v2"
 	"github.com/shixinshuiyou/mayo-dev/app/user/router"
 )
 
 func main() {
+	reg := etcdv3.NewRegistry(func(op *registry.Options) {
+
+	})
 	service := web.NewService(
 		web.Name("czh.micro.api.action"),
 		web.Address("127.0.0.1:9000"),
 		web.Handler(router.Register()),
+		web.Registry(reg),
 	)
 
 	service.Init()
